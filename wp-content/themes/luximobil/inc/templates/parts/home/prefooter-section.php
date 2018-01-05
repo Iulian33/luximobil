b20f83983611fd58dcf8b3091155af02ab9ae6c0
Julian Hook <cirligiuilan12312@gmail.com>
2018-01-05 12:09:27 +0200

    started prefooter module on homepage

--- wp-content/themes/luximobil/inc/templates/parts/home/prefooter-section.php
+++ wp-content/themes/luximobil/inc/templates/parts/home/prefooter-section.php
@@ -1,1 +1,12 @@
-prefooter
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <?php include ('prefooter-items/loturi-teren.php');?>
+            </div>
+            <div class="col-sm-6">
+                <?php include ('prefooter-items/spatii-comerciale.php');?>
+            </div>
+        </div>
+    </div>
+</section>
