8704e710a7b39f23370e0f9bf23a866aee8d0467
Julian Hook <cirligiuilan12312@gmail.com>
2018-01-06 03:21:40 +0200

    resolve all missing styles and restore merge project

--- wp-content/themes/luximobil/inc/templates/parts/top-slider-image.php
+++ wp-content/themes/luximobil/inc/templates/parts/top-slider-image.php
@@ -1,6 +1,4 @@
-
-
-<?php global $post?>
+<?php global $post ?>
 
 <?php var_dump(wp_get_attachment_image($post->ID)); ?>
 <div class="headerImage">
@@ -9,7 +7,14 @@
         if ($pageImage) { ?>
             <div class="pageImage" style="background-image: url(<?php echo $pageImage['url']; ?>);"></div>
         <?php } else if (is_singular('blog')) { ?>
-            <div class="pageImage" style="background-image:url(<?php echo get_the_post_thumbnail_url($post->ID); ?>); "></div>
+            <?php if (get_the_post_thumbnail_url($post->ID)) { ?>
+                <div class="pageImage"
+                     style="background-image:url(<?php echo get_the_post_thumbnail_url($post->ID); ?>); ">
+                </div>
+            <?php } else { ?>
+                <?php $defaultImage = get_field('standard_header_image', 'option'); ?>
+                <div class="pageImage" style="background-image: url(<?php echo $defaultImage['url']; ?>);"></div>
+            <?php } ?>
         <?php } else { ?>
             <?php $defaultImage = get_field('standard_header_image', 'option'); ?>
             <div class="pageImage" style="background-image: url(<?php echo $defaultImage['url']; ?>);"></div>
