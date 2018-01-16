9a3a0b316392be1cc8f9d30880fb647d0697723c
Skyrockerrz <crudu-alexei@mail.ru>
2018-01-16 23:33:26 +0200

    added and styled the product description list + description

--- wp-content/themes/luximobil/single.php
+++ wp-content/themes/luximobil/single.php
@@ -9,73 +9,28 @@ get_header(); ?>
 
 <div id="primary" class="mainContent">
     <div class="container">
-        <div class="carousel-container col-sm-7 col-lg-7">
-            <div class="swiper-container gallery-top">
-                <div class="swiper-wrapper">
-                    <?php if (have_rows('imobil_carousel')):
-
-                        while (have_rows('imobil_carousel')) : the_row(); ?>
-
-                            <?php $imobil_image = get_sub_field('imobil_image'); ?>
-                            <div class="swiper-slide"
-                                 style="background-image: url(<?php echo $imobil_image['url']; ?>);"></div>
-                        <?php endwhile; ?>
-                    <?php else : ?>
-                        // no rows found
-                    <?php endif; ?>
-                </div>
-                <!-- Add Arrows -->
-                <div class="swiper-button-next swiper-button-white"></div>
-                <div class="swiper-button-prev swiper-button-white"></div>
-            </div>
+        <?php get_template_part('content', 'single'); ?>
 
 
-            <div class="swiper-container gallery-thumbs">
-                <div class="swiper-wrapper">
-                    <?php if (have_rows('imobil_carousel')): ?>
-
-                        <?php while (have_rows('imobil_carousel')) : the_row(); ?>
-                            <?php $imobil_image = get_sub_field('imobil_image'); ?>
-                            <div class="swiper-slide swiper-slide-bottom"
-                                 style="background-image: url(<?php echo $imobil_image['url']; ?>);"></div>
-
-                            <?php the_sub_field('sub_field_name'); ?>
-
-                        <?php endwhile; ?>
-
-                    <?php else : ?>
-
-                        // no rows found
-
-                    <?php endif; ?>
-
-                </div>
-            </div>
-
-        </div>
         <div class="row">
-            <div class="col-xs-12 col-sm-7 col-lg-5">
-                <main id="main" class="site-main" role="main">
+            <div class="col-xs-12 col-sm-7 col-lg-7">
                     <?php JH_check_sidebar(); ?>
                     <?php while (have_posts()) : the_post(); ?>
-
-                        <?php get_template_part('content', 'single'); ?>
-
-                        <?php the_post_navigation(); ?>
-
-                        <?php
-                        // If comments are open or we have at least one comment, load up the comment template
-                        if (comments_open() || get_comments_number()) :
-                            comments_template();
-                        endif;
-                        ?>
+                    <div class="imobil-description">
+                        <h3 class="description-heading">
+                            <?php _e('Descriere', 'jhfw'); ?>
+                        </h3>
+                        <div class="description">
+                            <?php the_content();?>
+                        </div>
+                    </div>
 
                     <?php endwhile; // end of the loop. ?>
                     <?php JH_check_sidebar("end"); ?>
-                </main><!-- #main -->
             </div>
         </div>
     </div>
+
 </div><!-- #primary -->
 
 
