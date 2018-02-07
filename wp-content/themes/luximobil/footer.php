528dd2a73b197b0bf9e922071807b9de11ffd9e7
Julian Hook <cirligiuilan12312@gmail.com>
2018-02-07 02:32:13 +0200

    try to fix W3

--- wp-content/themes/luximobil/footer.php
+++ wp-content/themes/luximobil/footer.php
@@ -8,31 +8,31 @@
  */
 ?>
 
-<?php $footer_bg_img = get_field('footer_background_image','option'); ?>
+<?php $footer_bg_img = get_field('footer_background_image', 'option'); ?>
 
-<footer class="mainFooter" style="background-image: url(<?php echo $footer_bg_img['url'];?>);">
+<footer class="mainFooter" style="background-image: url(<?php echo $footer_bg_img['url']; ?>);">
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-sm-4 col-widget">
                 <div class="bg-contacts"></div>
                 <div class="contacts-contaner">
-                    <?php dynamic_sidebar( 'footer-contacts' ); ?>
+                    <?php dynamic_sidebar('footer-contacts'); ?>
                 </div>
             </div>
             <div class="col-md-9 col-sm-8">
                 <div class="row">
                     <div class="col-md-3 col-sm-6 col-widget menu-1">
-                        <?php dynamic_sidebar( 'footer-menu-1' ); ?>
+                        <?php dynamic_sidebar('footer-menu-1'); ?>
                     </div>
                     <div class="col-md-3 col-sm-6 col-widget menu-2">
-                        <?php dynamic_sidebar( 'footer-menu-2' ); ?>
+                        <?php dynamic_sidebar('footer-menu-2'); ?>
                     </div>
                     <div class="col-sm-8 col-widget footer-about">
                         <?php dynamic_sidebar('footer-about'); ?>
                     </div>
                     <div class="col-sm-12 credits-bttom-footer">
                         <span class="copy-right-text">
-                            <?php _e('© Copyright')?>
+                            <?php _e('© Copyright') ?>
                             <?php echo date("Y"); ?>
                         </span>
                         <span class="credits-link">
@@ -48,5 +48,6 @@
 
 <div id="mobilemenu" class="displayNone"></div>
 <?php wp_footer(); ?>
+</div>
 </body>
 </html>
